7169405ba1403d72c7259df6bfdbe895470c240b
marisanesmith <marisa@thisworldtraveled.com>
2021-03-22 22:50:11 -0400

    added in connection.js, server.js and some of the initial files in seeds and models and utils

---
+++ seeds/index.js
@@ -0,0 +1,24 @@
+const sequelize = require('../config/connection');
+// const { User, Blog, Comment } = require('../models');
+
+const seedComment = require('./commentData');
+const seedPost = require('./postData');
+const seedUser = require('./userData');
+
+const seedAll = async () => {
+    await sequelize.sync({ force: true });
+    console.log('\n----- DATABASE SYNCED ----\n');
+
+    await seedUser();
+    console.log('\n-----USER SEEDED----\n');
+
+    await seedPost();
+    console.log('\n-----POST SEEDED----\n');
+
+    await seedComment();
+    console.log('\n-----COMMENT SEEDED----\n');
+
+    process.exit(0);
+};
+
+seedAll();
