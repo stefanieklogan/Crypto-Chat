9d671efa67e216969b2168ce08cb0e9b8082d0e4
rsco <RaquelLee@users.noreply.github.com>
2021-03-25 18:24:23 -0400

    3rd party api routes attempted

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
 const commentRoutes = require('./commentRoutes');
+const cryptoRoutes = require('./cryptoRoutes');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
+router.use('/crypto', cryptoRoutes);
 
 
 module.exports = router;
