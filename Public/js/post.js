5aabc18ef40c004b541a513778852f5a0c401e77
marisanesmith <marisa@thisworldtraveled.com>
2021-03-27 12:13:01 -0400

    not to be saved

--- Public/js/post.js
+++ Public/js/post.js
@@ -5,7 +5,7 @@ const newFormHandler = async (event) => {
     const description = document.querySelector('#post-description').value.trim();
   
     if (name && description) {
-      const response = await fetch(`/api/post`, {
+      const response = await fetch('/api/post', {
         method: 'POST',
         body: JSON.stringify({ name, description }),
         headers: {
