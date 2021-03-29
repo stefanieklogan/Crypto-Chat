8d3ae1ec9c061fe607d04ba7c742155495038261
marisanesmith <marisa@thisworldtraveled.com>
2021-03-28 22:12:46 -0400

    changes

--- Public/js/login.js
+++ Public/js/login.js
@@ -4,7 +4,7 @@ const loginFormHandler = async (event) => {
     const email = document.querySelector('#email-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
   
-    // if (email && password) {
+    if (email && password) {
       const response = await fetch('/api/users/login', {
         method: 'POST',
         body: JSON.stringify({ email, password }),
@@ -16,7 +16,7 @@ const loginFormHandler = async (event) => {
       } else {
         alert('Failed to log in.');
       }
-    // }
+    }
   };
   
   document
