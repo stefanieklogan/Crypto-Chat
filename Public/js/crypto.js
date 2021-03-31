7f3ede735dcc51fa911b88fe368be3833f02c639
rsco <RaquelLee@users.noreply.github.com>
2021-03-31 09:48:18 -0400

    logic for api data retrieval

---
+++ Public/js/crypto.js
@@ -0,0 +1,12 @@
+const idArray = () => {
+    idArr = [];
+    for (i=0; i < 51; i++) {
+        idArr.push(data[i].id) 
+    }
+}
+
+const symSet = (sym) => {
+    symArr = [];
+    symArr.push(sym)
+}
+module.exports = symSet;
