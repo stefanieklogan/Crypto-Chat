675752155d9b0621755ce029c363428c7704ed1e
rsco <RaquelLee@users.noreply.github.com>
2021-04-01 15:35:50 -0400

    useful comments added

--- controllers/api/cryptoRoutes.js
+++ controllers/api/cryptoRoutes.js
@@ -6,7 +6,6 @@ var arr = [];
 
 // this route is to retrieve coin data for display in views/coinBar.HB
 
-// this route works to retrieve data and log (line 30) but doesn't display in insomnia - loading, loading...
 router.get("/", async (req, res) => {
     fetch("https://coinpaprika1.p.rapidapi.com/coins/", //ranked coins
         {
@@ -14,7 +13,7 @@ router.get("/", async (req, res) => {
                 "x-rapidapi-key": process.env.CP_API_KEY,
                 "x-rapidapi-host": "coinpaprika1.p.rapidapi.com"
             }
-        }).then(res => res.json()) // convert to json(express)
+        }).then(res => res.json())
             .then(data => { // all coin metadata returned (in order)
                 for(i=0; i<3; i++){ // only want to display some
                     fetch("https://coinpaprika1.p.rapidapi.com/coins/" + data[i].id, // grabs one coin id from metadata
@@ -23,19 +22,15 @@ router.get("/", async (req, res) => {
                         "x-rapidapi-key": process.env.CP_API_KEY,
                         "x-rapidapi-host": "coinpaprika1.p.rapidapi.com"
                         }
-                    }).then(res => res.json()) // convert to json
+                    }).then(res => res.json()) 
                         .then(data => { // 1 coin metadata by id
                             var sym = (data.symbol); // 1 symbol saved into sym since dot notation was confusing .push
                             arr.push(sym), // push 1 symbol into array
-                            console.log(arr),
                             fs.writeFile('arr.json', arr.join(','), 'utf-8', function (err) {
                                 if (err) return console.log(err);
                                 console.log('works');
                                 }
-                            ) //getArray();
-                            //WORKS BUT CANT BE IN LOOP?
-                             // each iteration of arr, completed at end. extract completed array
-                            // to hand to function below. issue- arr with proper value only exists here. 
+                            ) 
                         })
                 }; getArray()
             })
@@ -43,20 +38,20 @@ const getArray = () => {
     fs.readFile("arr.json", 'utf-8',function(err, data) {
         if (err) throw err;     
         const arr = data;
-        console.log("getarray",data); 
-        fetch("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=" + arr + "&tsyms=USD") // this api accepts comma separated string data, place arr where "BTC" is
+        console.log("array from arr.json", arr); 
+        fetch("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=" + arr + "&tsyms=USD") // full data for several coins
         .then(response => response.json())
         .then(data => {
-            const stuff = data.DISPLAY;
-            const manipulated = Object.entries(stuff).reduce((acc,el) => { // this fn turns api res into arr of objects
-                Object.entries(el[1]).forEach((stuff) => {
-                    stuff[1].SYM = el[0]
+            const display = data.DISPLAY; // api res for display
+            const manipulated = Object.entries(display).reduce((acc,el) => { // this fn turns api res into arr of objects
+                Object.entries(el[1]).forEach((display) => {
+                    display[1].SYM = el[0]
                     acc.push(stuff[1])
                 })
                 return acc;
             },[]) 
-            console.log(manipulated) // the array of objects to finally display in HB
-            res.render('coinBar', manipulated)
+            console.log(manipulated) // the array of objects to finally display in HB, WORKS
+            res.render('coinBar', manipulated) // attempting to use manipulated (array) in coinBar.HB without success (yet)
             }
         )
         });
