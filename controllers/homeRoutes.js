4f9a829f38c12172e94c139d3147340d21eb041d
marisanesmith <marisa@thisworldtraveled.com>
2021-03-31 20:02:52 -0400

    chat route works again

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -13,10 +13,13 @@ router.get('/', withAuth, async (req, res) => {
 //   });
 // });
 
-router.get('/post', withAuth, async (req, res) => {
+router.get('/post', async (req, res) => {
+  let postData = await Post.findAll({
+  });
   const posts = postData.map((post) => post.get({ plain: true }));
   res.render('post', { posts, logged_in: req.session.logged_in});
 });
+
 
 router.get('/dashboard', withAuth, async (req, res) => {
   const postData = await Post.findAll({
