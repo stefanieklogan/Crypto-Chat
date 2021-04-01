44522b5f120f77e5031137b6dd5fc281e869188d
marisanesmith <marisa@thisworldtraveled.com>
2021-03-31 20:42:53 -0400

    styling

--- Public/js/post.js
+++ Public/js/post.js
@@ -5,13 +5,13 @@ const newFormHandler = async (event) => {
     const title = document.querySelector('#post-Title').value.trim();
     const content = document.querySelector('#post-description').value.trim();
   
-    if (title && content) {
+    // if (title && content) {
       const response = await fetch('/api/post', {
         method: 'POST',
         body: JSON.stringify({ title: title, content: content }),
         headers: {
           'Content-Type': 'application/json',
-        },
+        // },
       });
 
       if (response.ok) {
