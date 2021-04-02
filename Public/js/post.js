9b120eee0c4b638b722ff83239e064baa343b3ca
marisanesmith <marisa@thisworldtraveled.com>
2021-04-02 16:17:19 -0400

    chat now works and can post to database and display on dashboard

--- Public/js/post.js
+++ Public/js/post.js
@@ -6,7 +6,7 @@ const newFormHandler = async (event) => {
     const content = document.querySelector('#post-description').value.trim();
   
     if (title && content) {
-      const response = await fetch('/api/post', {
+      const response = await fetch(`/api/posts`, {
         method: 'POST',
         body: JSON.stringify({ title: title, content: content }),
         headers: {
