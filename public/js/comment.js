b480904edcb5fad85d797bcde98d0045ed11ce31
marisanesmith <marisa@thisworldtraveled.com>
2021-04-03 12:14:59 -0400

    added public folder

---
+++ public/js/comment.js
@@ -0,0 +1,51 @@
+const displayCommentInput = async (event) => {
+  console.log('here');
+  event.preventDefault();
+  var commentBtn = document.getElementById('commentBtn');
+  var commentContainer = document.getElementById('commentContainer');
+
+  commentContainer.classList.remove('hide');
+  commentBtn.classList.add('hide');
+  console.log('commentBtn pressed');
+};
+
+document
+.querySelector('#commentBtn')
+.addEventListener('click', displayCommentInput);
+
+/////////////////////////////////////////////////////////
+
+const saveNewComment = async (event) => {
+  event.preventDefault();
+  console.log('saveBtn pressed');
+
+  const comment = document.querySelector('#commentText').value.trim();
+  const id = event.target.getAttribute('data-id');
+
+console.log(comment);
+console.log(id);
+
+
+  if (comment && id ) {
+    const response = await fetch('/api/comments', {
+      method: 'POST',
+      body: JSON.stringify({ comment, post_id: id }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      location.reload();
+                console.log('comment saved');
+
+
+    } else {
+      alert('Failed to save comment');
+    }
+  }
+};
+
+document
+.querySelector('.saveCommentBtn')
+.addEventListener('click', saveNewComment);
---
+++ public/js/signup.js
@@ -0,0 +1,27 @@
+const signupFormHandler = async function (event) {
+    event.preventDefault();
+  
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (username && email && password) {
+      const response = await fetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({ name: username, email: email, password: password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+     
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to sign up.');
+      }
+    }
+  };
+
+
+
+  document
+    .querySelector('#signup-form')
+    .addEventListener('click', signupFormHandler);
