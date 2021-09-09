8f1ae291171ea4efb25a9866b85147cb4ec5dfa0
rsco <RaquelLee@users.noreply.github.com>
2021-09-09 16:29:16 -0400

    date formatting

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,9 +1,9 @@
 module.exports = {
-    format_date: (date) => {
-      return date.toLocaleTimeString();
-    },
-    format_date: (date) => {
-      return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()} @ ${new Date(date).getHours()}:${new Date(date).getMinutes()
+  format_date: (date) => {
+    return date.toLocaleTimeString();
+  },
+  format_date: (date) => {
+    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()} @ ${new Date(date).getHours()}:${new Date(date).getMinutes()
       }`;
-    },
-  };
+  },
+};
